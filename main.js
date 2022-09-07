49d4c30626ded2f8c9af42b099a4868c4cd5459b
Min Khant Saw <minkhantsaw04@gmail.com>
2022-09-07 12:19:56 +0630

    Add New Line Html Year Line Js Var y d h m s

---
+++ main.js
@@ -0,0 +1,5 @@
+const years = document.querySelector(".years"),
+  day = document.querySelector(".days"),
+  hours = document.querySelector(".hours"),
+  minutes = document.querySelector(".minutes"),
+  seconds = document.querySelector(".seconds");
