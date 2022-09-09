6308f33589681ab38be3df32f1610daf101b2551
Min Khant Saw <minkhantsaw04@gmail.com>
2022-09-09 17:53:52 +0630

    test

--- main.js
+++
@@ -1,5 +0,0 @@
-const years = document.querySelector(".years"),
-  day = document.querySelector(".days"),
-  hours = document.querySelector(".hours"),
-  minutes = document.querySelector(".minutes"),
-  seconds = document.querySelector(".seconds");
