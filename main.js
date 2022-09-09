75d449d5500cdda47de4dfd8c84b72029cb0d788
Min Khant Saw <minkhantsaw04@gmail.com>
2022-09-09 17:38:30 +0630

    done js

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 const hours = document.querySelector(".hour"),
-  minutes = document.querySelector(".minute");
+  minutes = document.querySelector(".minute"),
+  dayAndNight = document.querySelector(".day-and-night");
 
-const containerDate = new Date();
+const date = new Date();
+let dn = null;
 
 setInterval(() => {
-  let hour = containerDate.getHours(),
-    minute = containerDate.getMinutes();
+  let hour = date.getHours(),
+    minute = date.getMinutes();
   if (hour > 12) {
     hour -= 12;
+    dn = "pm";
+  } else {
+    dn = "am";
   }
   hours.innerHTML = hour;
   minutes.innerHTML = minute;
+  dayAndNight.innerHTML = dn;
 }, 1000);
