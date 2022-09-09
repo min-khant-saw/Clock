a7675f4e6e7e2b41622e28801816003e053be52e
Min Khant Saw <minkhantsaw04@gmail.com>
2022-09-09 16:36:36 +0630

    success js

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-const hour = document.querySelector(".hour"),
-  minute = document.querySelector(".minute");
+const hours = document.querySelector(".hour"),
+  minutes = document.querySelector(".minute");
+
+const containerDate = new Date();
+
+setInterval(() => {
+  let hour = containerDate.getHours(),
+    minute = containerDate.getMinutes();
+  if (hour > 12) {
+    hour -= 12;
+  }
+  hours.innerHTML = hour;
+  minutes.innerHTML = minute;
+}, 1000);
